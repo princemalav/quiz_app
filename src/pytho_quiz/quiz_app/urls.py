9319613b97f8f560ccd6e1b5c37e1cc4cb0023ec
princemalav <33306779+princemalav@users.noreply.github.com>
2019-01-22 18:06:40 +0530

    added login and register method

--- src/pytho_quiz/quiz_app/urls.py
+++ src/pytho_quiz/quiz_app/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 from . import views
+
+app_name = 'quiz_app'
 urlpatterns = [
-    url(r'homepage',views.homepageview)
+    url(r'homepage',views.homepageview,name='homepage'),
+    url(r'register',views.register,name='register'),
+    url(r'login',views.user_login,name='login')
 ]
--- src/pytho_quiz/quiz_app/views.py
+++ src/pytho_quiz/quiz_app/views.py
@@ -1,10 +1,53 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render , reverse
+from django.shortcuts import HttpResponse , HttpResponseRedirect
+from .forms import UserRegistrationfrom
+from django.contrib.auth import login , authenticate , logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepageview(request):
     my_dic = {'insert':'Hello'}
     return render(request,'quiz_app/homepage.html',context=my_dic)
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserRegistrationfrom(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserRegistrationfrom()
+    return render(request,'quiz_app/register.html',{'user_form':user_form,
+                                                    'registered':registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponse('loged in')
+            else:
+                return HttpResponse('account not active')
+        else:
+            return HttpResponse('invalid login details')
+    else:
+        return render(request,'quiz_app/login.html')
+
+
+
+    return render(request,'quiz_app/login.html')
